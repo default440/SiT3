fa8a687ccc6422e30ca27990fc1d22fc931b21f2
default440 <62350024+default440@users.noreply.github.com>
2021-04-24 23:51:11 +0700

    Add files via upload

---
+++ SiT3/Browser/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Browser
+{
+    public partial class Form1 : Form
+    {
+        private Socket socket = null;
+        
+        public string address = "http://localhost:5000/";
+        public Uri uri = new Uri("http://localhost:5000/");
+
+        public List<Uri> Uris = new List<Uri>();
+        public int index = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            webBrowser1.ScriptErrorsSuppressed = true;
+
+            connectToUri(true);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void webBrowser1_Navigating(object sender, WebBrowserNavigatingEventArgs e)
+        {
+            if (e.Url.LocalPath != "blank")
+            {
+                uri = new Uri(address + e.Url.AbsolutePath.Substring(1, e.Url.AbsolutePath.Length - 1));
+
+                connectToUri(true);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void connectToUri(bool inc)
+        {
+            if (inc)
+            {
+                Uris.Add(uri);
+                index += 1;
+            }
+
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Connect(uri.Host, uri.Port);
+
+            string str = "GET " + uri.PathAndQuery + " HTTP/1.1\r\n" + "Host: " + uri.Host + "\r\n\r\n";
+
+            socket.Send(Encoding.ASCII.GetBytes(str));
+
+            byte[] bytes = new byte[4096];
+
+            _ = socket.Receive(bytes);
+
+            var str1 = Encoding.ASCII.GetString(bytes);
+
+            str1 = str1.Substring(str1.IndexOf('<'), str1.Length - 1 - str1.IndexOf('<'));
+
+            webBrowser1.DocumentText = str1;
+            socket.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (index > 1)
+            {
+                index -= 1;
+
+                uri = Uris[index - 1];
+
+                connectToUri(false);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (index < Uris.Count)
+            {
+                index += 1;
+
+                uri = Uris[index - 1];
+
+                connectToUri(false);
+            }
+        }
+    }
+}
---
+++ SiT3/SiT3/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SiT3
+{
+    public partial class Form1 : Form
+    {
+        private string link = null;
+        
+        private Socket active = null;
+        private Socket passive = null;
+
+        private byte[] bytes = null;
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = "";
+            active = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            passive = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            active.Connect("127.0.0.1", 21);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bytes = new byte[256];
+            var bytes2 = new byte[1024];
+
+            if (textBox1.Text != "") active.Send(Encoding.ASCII.GetBytes(textBox1.Text + "\r\n"));
+            if (passive.Connected) _ = passive.Receive(bytes2);
+
+            _ = active.Receive(bytes);
+
+            var rcv = Encoding.ASCII.GetString(bytes);
+
+            if (rcv.Substring(0, 3) == "227") openPassive(rcv);
+
+            label1.Text += rcv + "\n";
+            label2.Text = Encoding.ASCII.GetString(bytes2);
+        }
+
+        private void openPassive(string rcv)
+        {
+            rcv = rcv.Substring(rcv.IndexOf('(') + 1, (rcv.IndexOf(')') - rcv.IndexOf('(') - 1));
+            var strs = rcv.Split(',');
+
+            passive.Connect(strs[0] + "." + strs[1] + "." + strs[2] + "." + strs[3], Convert.ToInt32(strs[4])*256 + Convert.ToInt32(strs[5]));
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            link = textBox1.Text;
+        }
+    }
+}
